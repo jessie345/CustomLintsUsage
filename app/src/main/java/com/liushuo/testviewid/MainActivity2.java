69d3af0a029bb15779d0b2447b3e90a2057f33af
liushuo <mrshuo@sina.cn>
2017-05-10 19:49:48 +0800

    bug fix

---
+++ app/src/main/java/com/liushuo/testviewid/MainActivity2.java
@@ -0,0 +1,19 @@
+package com.liushuo.testviewid;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
+
+public class MainActivity2 extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        LayoutInflater.from(this);
+        LayoutInflater.from(this);
+        LayoutInflaterWrapper.from();
+
+    }
+}
---
+++ testlib/src/main/java/com/liushuo/testlib/MyLibAdapter.java
@@ -0,0 +1,27 @@
+package com.liushuo.testlib;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ * user liushuo
+ * date 2017/4/24
+ */
+
+public class MyLibAdapter extends RecyclerView.Adapter {
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
